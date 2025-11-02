45958e0326633364ae7ef4ccdd3ef681b8f65969
critiancastillo74 <cristian74425@gmail.com>
2025-11-02 16:29:23 -0600

    navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
 
 export default function Navbar() {
   return (
@@ -7,16 +7,50 @@ export default function Navbar() {
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost normal-case text-xl">
           Heladería
-          </Link>
+        </Link>
       </div>
-      <div className="flex-none gap-2 ">
-        <Link to="/reports/sales" className="btn btn-ghost">Reporte de Ventas</Link>
-        <Link to="/sales" className="btn btn-ghost">Registrar Venta</Link>
-        <Link to="/expenses" className="btn btn-ghost">Gastos</Link>
-        <Link to="/cylinder" className="btn btn-ghost">Cilindros</Link>
-        <Link to="/product" className="btn btn-ghost">Productos</Link>
-        <Link to="/cyInvent" className="btn btn-ghost">Inventario de helados</Link>
-        <Link to="/proInvent" className="btn btn-ghost">Inventario de productos</Link>
+
+      <div className="flex-none gap-2">
+        {/* Menú desplegable de Reportes */}
+        <div className="dropdown dropdown-hover">
+          <label tabIndex={0} className="btn btn-ghost m-1">
+            Reporteria
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-blue-600 rounded-box w-52"
+          >
+            <li>
+              <Link to="/reports/sales">Reporte de ventas</Link>
+            </li>
+            <li>
+              <Link to="/reports/sales/weekly">Reporte Semanal</Link>
+            </li>
+            <li>
+              <Link to="/reports/sales/monthly">Reporte Mensual</Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Resto de los botones */}
+        <Link to="/sales" className="btn btn-ghost">
+          Registrar Venta
+        </Link>
+        <Link to="/expenses" className="btn btn-ghost">
+          Gastos
+        </Link>
+        <Link to="/cylinder" className="btn btn-ghost">
+          Cilindros
+        </Link>
+        <Link to="/product" className="btn btn-ghost">
+          Productos
+        </Link>
+        <Link to="/cyInvent" className="btn btn-ghost">
+          Inventario de helados
+        </Link>
+        <Link to="/proInvent" className="btn btn-ghost">
+          Inventario de productos
+        </Link>
       </div>
     </div>
   );
